26fb3fb13080407959faeb005f6ca5cebfb01796
serenasangeunlee <serena.lee@kellogg.northwestern.edu>
2017-05-01 19:53:49 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get("/", { :controller => "pictures", :action => "index" })
   # # CREATE
   get("/photos/new", { :controller => "pictures", :action => "new_form" })
   get("/create_photo", { :controller => "pictures", :action => "create_row" })
